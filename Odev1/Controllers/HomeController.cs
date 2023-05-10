e14467cf50d886b0deefa108dc49b6f777e3f2df
aleynakuvan <aleynakuvann@icloud.com>
2023-05-10 11:59:12 +0300

    Add project files.

---
+++ Odev1/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using Odev1.Data;
+using Odev1.Entities;
+using Odev1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Odev1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        ProjectContext db = new ProjectContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel user)
+        {
+           
+            User model = db.Users.FirstOrDefault(i => i.UserName == user.UserName && i.Password == user.Password);
+            if (model != null)
+            {
+                return RedirectToAction("Home");
+            }
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = db.Users.FirstOrDefault(i => i.UserName == model.UserName || i.Email == model.Email);
+                if (user == null)
+                {
+                    user = new User()
+                    {
+                        UserName = model.UserName,
+                        Password = model.RePassword,
+                        Email = model.Email,
+                        FirstName = model.FirstName,
+                        LastName = model.LastName
+                    };
+                    db.Users.Add(user);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    return View(model);
+                }
+
+            }
+            return View(model);
+
+        }
+
+    }
+}
---
+++ Odev1/Data/ProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using Odev1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Odev1.Data
+{
+    
+        public class ProjectContext : DbContext
+        {
+            public ProjectContext() : base("Server=MSI\\SQLEXPRESS; Database=WebOdev;integrated security=True;") 
+            {
+            }
+            public DbSet<User> Users { get; set; }
+        }
+    
+}
